2187bb9893762d97b399560bcd177d88bac60e6a
Ely Lucas <ely@meta-tek.net>
2022-08-16 12:49:21 -0600

    removing log

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,8 +4,6 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-console.log(process.env);
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Cypress for Angular Devs Workshop',

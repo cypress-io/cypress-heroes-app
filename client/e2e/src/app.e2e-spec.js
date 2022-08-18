1809b967ca6cfaea0a3f822aa822023b4c0eab0d
Ely Lucas <ely@meta-tek.net>
2022-08-18 08:55:01 -0600

    protractor updates

--- client/e2e/src/app.e2e-spec.js
+++ client/e2e/src/app.e2e-spec.js
@@ -1,21 +1,30 @@
-const { browser, by, element } = require('protractor')
+const { browser, by, element } = require("protractor");
 
-describe('Angular App', () => {
-    it('checks title', () => {
-        browser.get("/")
-        expect(browser.getTitle()).toEqual('CyHeroes')
-    })
+describe("Angular App", () => {
+  it("checks title", () => {
+    browser.get("/");
+    expect(browser.getTitle()).toEqual("CyHeroes");
+  });
 
-    it('can login', () => {
-        expect(element(by.id('login')).getText()).toEqual('Login')
-        browser.actions().click(element(by.id('login'))).perform()
-        element(by.id('login-email')).sendKeys('test@test.com')
-        element(by.id('login-password')).sendKeys('test123')
-        browser.actions().click(element(by.buttonText('Sign in'))).perform()
-        expect(element(by.id('logout')).getText()).toEqual('Logout')
-    })
+  it("can login", () => {
+    expect(element(by.id("login")).getText()).toEqual("Login");
+    browser
+      .actions()
+      .click(element(by.id("login")))
+      .perform();
 
-    it('displays heroes', () => {
-        expect(element.all(by.tagName('li')).count()).not.toEqual(0)
-    })
-})
+    element(by.id("login-email")).sendKeys("test@test.com");
+    element(by.id("login-password")).sendKeys("test123");
+
+    browser
+      .actions()
+      .click(element(by.buttonText("Sign in")))
+      .perform();
+
+    expect(element(by.id("logout")).getText()).toEqual("Logout");
+  });
+
+  it("displays heroes", () => {
+    expect(element.all(by.css("app-hero-list li")).count()).not.toEqual(0);
+  });
+});
